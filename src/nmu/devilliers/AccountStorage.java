b7385233db36c84cbb8abb44f09a0b59f02cc1d1
Jack <scruffydevilliers@gmail.com>
2018-07-18 23:26:39 +0200

    Working on Blockchain Working on DownloadBuff to automate getting all information in order to create a "Source" TODO: XML-Accounts -> WriteTo, ensure that no duplicates are present, no read ; Sources; Do Sources

--- src/nmu/devilliers/AccountStorage.java
+++ src/nmu/devilliers/AccountStorage.java
@@ -1,14 +1,6 @@
 package nmu.devilliers;
 
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
-import org.w3c.dom.ls.DOMImplementationLS;
-import org.w3c.dom.ls.LSOutput;
-import org.w3c.dom.ls.LSSerializer;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.FileOutputStream;
 import java.util.TreeSet;
 
 public class AccountStorage
